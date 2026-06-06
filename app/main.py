6414ca46f64d416459a15b6444706e9958bbe463
Yuanjie Li <yuanjie.li@utexas.edu>
2026-06-06 16:06:22 -0600

    [ADD] other table endpoints

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.engine import URL
 from sqlalchemy.orm import sessionmaker
 import sys
+from datetime import datetime
 from pathlib import Path
 
 # Add the current working directory to sys.path
@@ -108,4 +109,107 @@ async def update_devices(target_id: int,
     session.add(entry)
     session.commit()
     return {"Device updated": entry.id}
+
+#################
+### Locations ###
+#################
+@app.post("/create/locations")
+async def create_locations(new_plant_id: int ):
+    entry = Locations(plant_id=new_plant_id)
+    session.add(entry)
+    session.commit()
+    return {"Locations added": entry.id}
+
+@app.post("/update/locations")
+async def update_locations(target_id: int, 
+                        new_plant_id: int = None):
+    entry = session.query(Locations).filter(
+                          Locations.id == target_id).first()
+    
+    if new_plant_id is not None: 
+        entry.plant_id = new_plant_id
+    session.add(entry)
+    session.commit()
+    return {"Locations updated": entry.id}
+
+#################
+### Locations ###
+#################
+@app.post("/create/locations")
+async def create_locations(new_plant_id: int ):
+    entry = Locations(plant_id=new_plant_id)
+    session.add(entry)
+    session.commit()
+    return {"Locations added": entry.id}
+
+@app.post("/update/locations")
+async def update_locations(target_id: int, 
+                        new_plant_id: int = None):
+    entry = session.query(Locations).filter(
+                          Locations.id == target_id).first()
+    
+    if new_plant_id is not None: 
+        entry.plant_id = new_plant_id
+    session.add(entry)
+    session.commit()
+    return {"Locations updated": entry.id}
+
+#################
+# Measurements ##
+#################
+@app.post("/create/measurements")
+async def create_measurements(new_timestamp: datetime,
+                              new_value: float,
+                              new_device_id: int,
+                              new_location_id: int):
+    entry = Measurements(timestampe=new_timestamp,
+                         value=new_value,
+                         device_id=new_device_id,
+                         location_id=new_location_id)
+    session.add(entry)
+    session.commit()
+    return {"Measurements added": entry.id}
+
+@app.post("/update/measurements")
+async def update_measurements(target_id: int, 
+                              new_timestamp: datetime = None,
+                              new_value: float = None,
+                              new_device_id: int = None,
+                              new_location_id: int = None):
+    entry = session.query(Measurements).filter(
+                          Measurements.id == target_id).first()
+    
+    if new_timestamp is not None: 
+        entry.timestamp = new_timestamp
+    if new_value is not None: 
+        entry.value = new_value
+    if new_device_id is not None: 
+        entry.device_id = new_device_id
+    if new_location_id is not None: 
+        entry.location_id = new_location_id
+    session.add(entry)
+    session.commit()
+    return {"Peasurements updated": entry.id}
+
+#################
+#### Plants #####
+#################
+@app.post("/create/plants")
+async def create_plants(new_type: int ):
+    entry = Plants(type=new_type)
+    session.add(entry)
+    session.commit()
+    return {"Plants added": entry.id}
+
+@app.post("/update/plants")
+async def update_plants(target_id: int, 
+                        new_type: int = None):
+    entry = session.query(Plants).filter(
+                          Plants.id == target_id).first()
+    
+    if new_type is not None: 
+        entry.type = new_type
+    session.add(entry)
+    session.commit()
+    return {"Plants updated": entry.id}
 
